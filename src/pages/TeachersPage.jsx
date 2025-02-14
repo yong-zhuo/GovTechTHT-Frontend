ff5d9169d456aadcfe90aee7c6183e383759cc2e
yong-zhuo <yap.aloysious@gmail.com>
2025-02-15 00:11:04 +0800

    Refactor Teachers Page to fetch mock data

--- src/pages/TeachersPage.jsx
+++ src/pages/TeachersPage.jsx
@@ -1,16 +1,40 @@
-import { useMemo } from "react"
+import { useEffect, useMemo, useState } from "react"
 import PersonsPage from "./PersonsPage"
 import { convertTeachersData } from "../utils/tableDataUtils"
-import { teachers } from "../data/mockTeacherData"
 import { teacherTableColumns } from "../data/tableColumns"
+import { LuLoaderCircle } from "react-icons/lu"
 
 const TeachersPage = () => {
 
-  const teachersData = useMemo(() => convertTeachersData(teachers), [])
+  const [teachers, setTeachers] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchTeachers = async () => {
+      setIsLoading(true)
+
+      try {
+        const response = await fetch('/data/teachers.json')
+        const data = await response.json()
+        setTeachers(data)
+      } catch (e) {
+        alert('Error fetching students data')
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    fetchTeachers()
+  }, [])
+
+  const teachersData = useMemo(() => convertTeachersData(teachers), [teachers])
+
+  if (isLoading) {
+    return <div className="flex flex-row items-center text-slate-600 gap-1 h-screen justify-center text-3xl">Loading<LuLoaderCircle className="animate-spin" /></div>
+  }
 
   return (
     <>
-      <PersonsPage data={teachersData} tableColumns={teacherTableColumns}/>
+      <PersonsPage data={teachersData} tableColumns={teacherTableColumns} />
     </>
   )
 }
