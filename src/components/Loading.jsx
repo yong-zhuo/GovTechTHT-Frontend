ae501bd0163cd9b31959170b95a0d515ab77d3b6
yong-zhuo <yap.aloysious@gmail.com>
2025-02-15 00:18:07 +0800

    Add Loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import { LuLoaderCircle } from "react-icons/lu"
+
+const Loading = () => {
+    return (
+        <div className="flex flex-row items-center text-slate-600 gap-1 h-screen justify-center text-3xl">
+            Loading<LuLoaderCircle className="animate-spin" />
+        </div>
+    )
+}
+
+export default Loading
